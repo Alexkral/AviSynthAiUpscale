84e580f33b89d36fc38053842f7bfda4209fddb9
Alexkral <31992728+Alexkral@users.noreply.github.com>
2021-03-28 03:30:38 +0200

    Added new models

---
+++ Shaders/Photo/Fast Sharp/2x/src/depth2space.hlsl
@@ -0,0 +1,13 @@
+sampler s0 : register(s0);
+sampler s1 : register(s1);
+float2 p0 :  register(c0);
+float2 p1 :  register(c1);
+
+float4 main(float2 tex : TEXCOORD0) : COLOR
+{
+float2 fcoord = frac(tex * p0 * 0.5);
+float2 base = tex + (float2(0.5, 0.5) - fcoord) * p1 * 2.0;
+int2 index = int2(fcoord * float2(2, 2));
+float4 res = tex2D(s0, base);
+return float4(res[index.x * 2 + index.y] + tex2D(s1, tex)[0], 0, 0, 1);
+}
